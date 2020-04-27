b8a58f2754792a5fb6d4921da3f77db45b74b764
Nadezhda Komaritckaia <komaritskaya@Nadezhdas-MacBook-Pro.local>
2020-04-27 07:09:50 +0300

    Символический коммит

--- src/main.js
+++ src/main.js
@@ -22,3 +22,17 @@ const boardController = new BoardController(boardComponent);
 
 render(siteMainElement, boardComponent);
 boardController.render(tasks);
+
+//                                      /~\
+//                                     |oo )
+//                                     _\=/_
+//                     ___         #  /  _  \
+//                    / ()\        \\//|/.\|\\
+//                  _|_____|_       \/  \_/  ||
+//                 | | === | |         |\ /| ||
+//                 |_|  O  |_|         \_ _/ #
+//                  ||  O  ||          | | |
+//                  ||__*__||          | | |
+//                 |~ \___/ ~|         []|[]
+//                 /=\ /=\ /=\         | | |
+// ________________[_]_[_]_[_]________/_]_[_\_________________________
