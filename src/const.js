352cedf5cdc9bada0f7bf083c142167a7eef4b0d
Nadezhda Komaritckaia <komaritskaya@Nadezhdas-MBP.lan>
2020-05-14 13:56:45 +0300

    Добавлена модель данных и фильтрация

--- src/const.js
+++ src/const.js
@@ -1,4 +1,12 @@
-export const COLORS = [`black`, `yellow`, `blue`, `green`, `pink`];
+export const COLOR = {
+  BLACK: `black`,
+  YELLOW: `yellow`,
+  BLUE: `blue`,
+  GREEN: `green`,
+  PINK: `pink`,
+};
+
+export const COLORS = Object.values(COLOR);
 
 export const DAYS = [ `mo`, `tu`, `we`, `th`, `fr`, `sa`, `su` ];
 
@@ -17,4 +25,11 @@ export const MONTHS = [
   `December`,
 ];
 
-
+export const FilterType = {
+  ALL: `all`,
+  ARCHIVE: `archive`,
+  FAVORITES: `favorites`,
+  OVERDUE: `overdue`,
+  REPEATING: `repeating`,
+  TODAY: `today`,
+};
